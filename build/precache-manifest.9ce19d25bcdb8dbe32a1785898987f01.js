c08c30ca2c737a0f7bf54436e3448df287a99e86
george ward <gcward18@gmail.com>
2019-12-10 16:03:31 -0600

    fixed border in header

---
+++ build/precache-manifest.9ce19d25bcdb8dbe32a1785898987f01.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd8aadd6478f612134ae61e362a37f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "524b916970bd764c79cf",
+    "url": "/static/css/main.d67c4680.chunk.css"
+  },
+  {
+    "revision": "d2d82267b326807d0fbf",
+    "url": "/static/js/2.df1b982b.chunk.js"
+  },
+  {
+    "revision": "524b916970bd764c79cf",
+    "url": "/static/js/main.38c18aac.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/static/media/Facebook_Logo_(2019).5d6ea38a.png"
+  },
+  {
+    "revision": "b384e1600ccdd193ec4c66224990fd8a",
+    "url": "/static/media/FullJoe.b384e160.jpg"
+  },
+  {
+    "revision": "cc5e002f7bb3f6f3a515ad9fc2cf66fa",
+    "url": "/static/media/ds.cc5e002f.png"
+  },
+  {
+    "revision": "229079c8f5240851cece598cf8eee770",
+    "url": "/static/media/email-logo.229079c8.png"
+  },
+  {
+    "revision": "f08c524d147f46398cb54013ee7013ee",
+    "url": "/static/media/linkedin-Mark.f08c524d.png"
+  },
+  {
+    "revision": "449433a14be888c4a5c1694e7adf6a0b",
+    "url": "/static/media/personal_photo.449433a1.jpg"
+  },
+  {
+    "revision": "cf2f76c76911d6a6a2b931b1ac6f7432",
+    "url": "/static/media/www.cf2f76c7.png"
+  }
+]);
