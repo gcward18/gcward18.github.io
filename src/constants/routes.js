c0be04ffd13ce9f812b5118781384d3f78a7a8cd
George Charon Ward <gward@iperformx.com>
2025-04-18 15:31:30 -0400

    removing old content and migrating to angular

--- src/constants/routes.js
+++
@@ -1,7 +0,0 @@
-var base = '/'
-export const HOME=`${base}home`
-export const RESUME=`${base}resume`
-export const PROJECTS=`${base}projects`
-export const ABOUT=`${base}about`
-export const STATS=`${base}stats`
-export const CONTACT=`${base}contact`
